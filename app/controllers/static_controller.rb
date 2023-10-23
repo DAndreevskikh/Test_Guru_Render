0b95bbb0f9fbf9767edb0a8f8091a3a500cef114
DAndreevskikh <northside753@gmail.com>
2023-10-23 20:17:15 +0000

    Внесены следующие изменения по замечаниям:
    1. Дополнен список сущностей приложения.
    2. Статические html файлы перемещены в директорию public.
    3. Добавлены атрибуты target='_blank' и rel='nofollow noopener' к ссылкам на сторонние ресурсы.
    4. Добавлен файл ru.yml в config/locales, аналогичный en.yml.

--- app/controllers/static_controller.rb
+++
@@ -1,7 +0,0 @@
-class StaticController < ApplicationController
-  def about
-  end
-
-  def about_author
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
-  get 'about', to: 'static#about'
-  get '/about/author', to: 'static#about_author'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
