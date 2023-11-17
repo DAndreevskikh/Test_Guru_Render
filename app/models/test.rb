ca71046e3cf1553a26f89fb2b455fef9d98cdc63
DAndreevskikh <northside753@gmail.com>
2023-11-17 14:23:36 +0000

    Правки: - Добавлена модель UserTest для связи пользователей с тестами. - Исправлен метод tests_by_level  в модели User, используя связь с UserTest. - Обновлен метод tests_by_category  в модели Test для использования связи с Category.

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,6 @@
 class Test < ApplicationRecord
    def self.tests_by_category(category_name)
-    Test.joins(:category)
+    joins(:category)
         .where(categories: { name: category_name })
         .order(title: :desc)
         .pluck(:title)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-   def tests_by_level(level)
-    Test.joins("JOIN user_tests ON user_tests.test_id = tests.id")
-        .where("user_tests.user_id = ? AND tests.level = ?", self.id, level)
+  def tests_by_level(level)
+    Test.joins(:user_tests)
+        .where(user_tests: { user_id: self.id }, level: level)
   end
 end
