16a2e890e0f1321cc066e2f0cd78f0837fde3787
DAndreevskikh <northside753@gmail.com>
2023-11-22 19:09:00 +0000

    Добавлена ER-диаграмма и определены модельные ассоциации. - ER-диаграмма размещена в Wiki. - Ассоциации моделей проверены и функционируют корректно.

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,8 +1,14 @@
 class Test < ApplicationRecord
-   def self.tests_by_category(category_name)
+  belongs_to :category
+  has_many :questions
+  has_many :user_tests
+  has_many :users, through: :user_tests
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+
+  def self.tests_by_category(category_name)
     joins(:category)
-        .where(categories: { name: category_name })
-        .order(title: :desc)
-        .pluck(:title)
+      .where(categories: { name: category_name })
+      .order(title: :desc)
+      .pluck(:title)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
+  has_many :user_tests
+  has_many :tests, through: :user_tests
+  has_many :created_tests, class_name: 'Test', foreign_key: 'author_id'
+
   def tests_by_level(level)
     Test.joins(:user_tests)
-        .where(user_tests: { user_id: self.id }, level: level)
+        .where(user_tests: { user_id: id }, level: level)
   end
 end
