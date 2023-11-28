d1d0e1496136a34b8970d805d654b09278c2c570
DAndreevskikh <northside753@gmail.com>
2023-11-28 08:32:18 +0000

    Правки: - Добавлен dependent: :destroy в модели Category, Test, и Question; - Обновлены ассоциации согласно Rails Style Guide; - Упрощены методы в модели User.

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,9 +1,10 @@
 class Test < ApplicationRecord
   belongs_to :category
-  has_many :questions
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+
+  has_many :questions, dependent: :destroy
   has_many :user_tests
   has_many :users, through: :user_tests
-  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
   def self.tests_by_category(category_name)
     joins(:category)
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ApplicationRecord
   has_many :created_tests, class_name: 'Test', foreign_key: 'author_id'
 
   def tests_by_level(level)
-    Test.joins(:user_tests)
-        .where(user_tests: { user_id: id }, level: level)
+    tests.where(level: level)
   end
 end
