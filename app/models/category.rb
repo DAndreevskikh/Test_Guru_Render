755272d5e44c371a84d39521c4d033c7555f8c4a
DAndreevskikh <northside753@gmail.com>
2023-12-01 14:52:39 +0000

    Добавлены scope-методы и валидации в модели Answer, Category, Question, Test и User. Реализованы scopes для фильтрации ответов и тестов, а также валидации для атрибутов и уникальности тестов.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ApplicationRecord
   has_many :tests, dependent: :destroy
+
+  scope :sorted, -> { order(name: :asc) }
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,13 @@ class Question < ApplicationRecord
   belongs_to :test
 
   has_many :answers, dependent: :destroy
+
+  validates :body, presence: true
+  validate :validate_number_of_answers
+
+  private
+
+  def validate_number_of_answers
+    errors.add(:base, 'Number of answers should be between 1 and 4') if answers.count < 1 || answers.count > 4
+  end
 end
