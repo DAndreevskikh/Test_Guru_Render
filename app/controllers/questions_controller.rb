27d4f2c23889205ea3ed0b66139b40478c811598
DAndreevskikh <northside753@gmail.com>
2024-01-10 18:57:29 +0000

    Изменения:
    - Добавлены представления index.html.erb и new.html.erb для списка вопросов и формы создания нового вопроса.
    - Обновлен QuestionsController:
      - Создание вопросов через построение объекта в памяти перед сохранением.
      - Реализован метод new для инициализации нового вопроса.
      - Добавлена логика перенаправления после удаления вопроса в методе destroy.
    - Реализована обработка исключения ActiveRecord::RecordNotFound для несуществующих вопросов.
    - Маршруты обновлены для упрощения с использованием shallow: true.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,25 +1,30 @@
 class QuestionsController < ApplicationController
-  before_action :find_test, only: %i[index create destroy show]
+  before_action :find_test, only: %i[index create new]
   before_action :find_question, only: %i[show destroy]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
   def index
-    render plain: @test.questions.pluck(:body).join("\n")
+    @questions = @test.questions
+  end
+
+  def new
+    @question = @test.questions.build
   end
 
   def create
-    question = @test.questions.create(question_params)
-    if question.persisted?
-      redirect_to test_questions_path(@test)
+    @question = @test.questions.build(question_params)
+    if @question.save
+      redirect_to question_path(@question)
     else
       render :new
     end
   end
 
   def destroy
+    test_id = @question.test_id
     @question.destroy
-    redirect_to test_questions_path(@test), notice: 'Вопрос был удален.'
+    redirect_to test_questions_path(test_id), notice: 'Вопрос был удален.'
   end
 
   private
