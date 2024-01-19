8c7704eb08e0da6104fc4a965ffcd7fe4cdc0c55
DAndreevskikh <northside753@gmail.com>
2024-01-19 13:48:31 +0000

    Оптимизация контроллеров и представлений, удаление лишних маршрутов: -Убран экшен index из QuestionsController, список вопросов теперь отображается на странице теста. -Исправлены ссылки и формы в представлениях, удалены ненужные маршруты (test_questions_path). -Добавлены и оптимизированы хелперы для форм и футера. -Внесены улучшения в код в соответствии с принципами REST

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,22 +1,15 @@
 class QuestionsController < ApplicationController
   before_action :find_question, only: %i[show destroy edit update]
-  before_action :find_test, only: %i[index create new]
-
-  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
-
-  def index
-    @questions = @test.questions
-  end
+  before_action :find_test, only: %i[create new]
 
   def new
-    @test = Test.find(params[:test_id])
     @question = Question.new
   end
 
   def create
     @question = @test.questions.build(question_params)
     if @question.save
-      redirect_to question_path(@question)
+      redirect_to test_path(@question.test)
     else
       render :new
     end
@@ -24,27 +17,25 @@ class QuestionsController < ApplicationController
 
   def destroy
     @question.destroy
-    redirect_to test_questions_path(@question.test_id), notice: 'Вопрос был удален.'
+    redirect_to test_path(@question.test)
   end
 
   def edit
-    @question = Question.find(params[:id])
     @test = @question.test
   end
 
   def update
-  find_question
-  if @question.update(question_params)
-    redirect_to test_questions_path(@question.test), notice: 'Вопрос был обновлен.'
-  else
-    render :edit
+    if @question.update(question_params)
+      redirect_to test_path(@question.test), notice: 'Вопрос был обновлен.'
+    else
+      render :edit
+    end
   end
-end
 
   private
 
   def find_test
-    @test = @question.present? ? @question.test : Test.find(params[:test_id])
+    @test = Test.find(params[:test_id])
   end
 
   def find_question
--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,10 +1,47 @@
 class TestsController < ApplicationController
-  def show
-    @test = Test.find(params[:id])
-    @questions = @test.questions
-  end
+  before_action :set_test, only: %i[show edit update destroy]
 
   def index
     @tests = Test.all
+  end
+
+  def show
+    @questions = @test.questions
+  end
+
+  def new
+    @test = Test.new
+  end
+
+  def create
+    @test = Test.new(test_params)
+    if @test.save
+      redirect_to @test
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @test.update(test_params)
+      redirect_to @test
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @test.destroy
+    redirect_to tests_path
+  end
+
+  private
+
+  def set_test
+    @test = Test.find(params[:id])
+  end
+
+  def test_params
+    params.require(:test).permit(:title, :category_id, :level)
   end
 end
