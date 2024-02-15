569f520bb331035763ffc5b3373fba0358704bdb
Dmitry <dmitry@Ubuntu.myguest.virtualbox.org>
2024-02-15 16:01:13 +0300

    Задание_10

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,10 @@ module ApplicationHelper
   def github_url(author, repo)
     link_to repo, "https://github.com/#{author}/#{repo}", target: '_blank', rel: 'noopener'
   end
+
+  def flash_message(tag, key, css_class)
+    return unless flash[key]
+
+    content_tag(tag, flash[key], class: css_class)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,19 @@
 class User < ApplicationRecord
-  has_many :user_tests
-  has_many :test_passages
+  has_many :user_tests, dependent: :destroy
+  has_many :test_passages, dependent: :destroy
   has_many :tests, through: :test_passages
-  has_many :created_tests, class_name: 'Test', foreign_key: 'author_id'
+  has_many :created_tests, class_name: 'Test', foreign_key: 'author_id', dependent: :destroy
 
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :name, presence: true
 
-  def test_passage(test)
-    test_passages.order(id: :desc).find_by(test_id: test.id)
-  end
+  has_secure_password
 
   def tests_by_level(level)
     tests.where(level: level)
+  end
+
+  def test_passage(test)
+    test_passages.order(id: :desc).find_by(test_id: test.id)
   end
 end
