b3a209f0433a38f08915383169071d0f13e7516e
Dmitry <dmitry@Ubuntu.myguest.virtualbox.org>
2024-02-16 15:23:47 +0300

    Обновил seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,20 @@
-# Создание пользователей
-user1 = User.create(name: 'Алексей')
-user2 = User.create(name: 'Мария')
+# Создание пользователей с email и паролем
+user1 = User.create(name: 'Алексей', email: 'alexey@example.com', password: '123456', password_confirmation: '123456')
+user2 = User.create(name: 'Мария', email: 'maria@example.com', password: '123456', password_confirmation: '123456')
 
 # Создание категорий
 category = Category.create(name: 'Ruby')
 
-# Создание тестов
-test1 = Test.create(title: 'Основы Ruby', level: 1, category_id: category.id, author_id: user1.id)
-test2 = Test.create(title: 'Продвинутый Ruby', level: 2, category_id: category.id, author_id: user1.id)
+# Создание тестов с автором
+test1 = Test.create(title: 'Основы Ruby', level: 1, category: category, author: user1)
+test2 = Test.create(title: 'Продвинутый Ruby', level: 2, category: category, author: user1)
 
 # Создание вопросов
-question1 = Question.create(body: 'Что такое Ruby?', test_id: test1.id)
-question2 = Question.create(body: 'Что такое массив в Ruby?', test_id: test1.id)
+question1 = Question.create(body: 'Что такое Ruby?', test: test1)
+question2 = Question.create(body: 'Что такое массив в Ruby?', test: test1)
 
 # Создание ответов
-Answer.create(body: 'Язык программирования', correct: true, question_id: question1.id)
-Answer.create(body: 'Браузер', correct: false, question_id: question1.id)
-Answer.create(body: 'Список элементов, упорядоченных по индексу', correct: true, question_id: question2.id)
-Answer.create(body: 'Тип данных для текста', correct: false, question_id: question2.id)
+Answer.create(body: 'Язык программирования', correct: true, question: question1)
+Answer.create(body: 'Браузер', correct: false, question: question1)
+Answer.create(body: 'Список элементов, упорядоченных по индексу', correct: true, question: question2)
+Answer.create(body: 'Тип данных для текста', correct: false, question: question2)
