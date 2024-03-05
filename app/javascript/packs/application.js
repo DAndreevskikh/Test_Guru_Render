56cbf9232eb4ac02b2bc0ffd68d35b49666c0982
Dmitry <dmitry@Ubuntu.myguest.virtualbox.org>
2024-03-06 00:09:33 +0300

    Задание 13: Twitter Bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,11 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+import 'bootstrap'
+import $ from 'jquery'
+window.jQuery = $;
+window.$ = $;
+import "../stylesheets/application"
+
+import Popper from '@popperjs/core';
+window.Popper = Popper;
