3e6361ab0aa9368013318c6c1a6b58886ba49be1
dmitry <northside753@gmail.com>
2024-04-17 23:33:30 +0300

    Deploy

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,7 +1,9 @@
-set -0 errexit
+set -e errexit
+
+export NODE_OPTIONS=--openssl-legacy-provider
 
 bundle install
 yarn install
 bundle exec rake assets:precompile
-bundle exec rake assets:clean
+#bundle exec rake assets:clean
 bundle exec rake db:migrate
