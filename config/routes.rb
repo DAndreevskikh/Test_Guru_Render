5e142241ef862e84078aa997910788c5d631c73b
Dmitry <133801191+DAndreevskikh@users.noreply.github.com>
2024-04-24 11:55:38 +0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   scope '(:lang)', lang: /#{I18n.available_locales.join('|')}/ do
     root 'tests#index'
-
+    
     devise_for :users, path: :gurus, controllers: { sessions: 'sessions' },
                        path_names: { sign_in: :log_in, sign_out: :log_out }
 
@@ -25,5 +25,7 @@ Rails.application.routes.draw do
         end
       end
     end
+
+    resource :feedbacks, only: [:new, :create]
   end
 end
