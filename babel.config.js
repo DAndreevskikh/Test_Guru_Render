0d303bc601227a7695c76297b8482df96faeae49
dmitry <northside753@gmail.com>
2024-04-25 13:19:10 +0300

    Deploy

--- babel.config.js
+++ babel.config.js
@@ -8,10 +8,10 @@ module.exports = function(api) {
   if (!validEnv.includes(currentEnv)) {
     throw new Error(
       'Please specify a valid `NODE_ENV` or ' +
-      '`BABEL_ENV` environment variables. Valid values are "development", ' +
-      '"test", and "production". Instead, received: ' +
-      JSON.stringify(currentEnv) +
-      '.'
+        '`BABEL_ENV` environment variables. Valid values are "development", ' +
+        '"test", and "production". Instead, received: ' +
+        JSON.stringify(currentEnv) +
+        '.'
     );
   }
 
@@ -30,6 +30,7 @@ module.exports = function(api) {
         {
           forceAllTransforms: true,
           useBuiltIns: 'entry',
+          corejs: 3,
           modules: false,
           exclude: ['transform-typeof-symbol']
         }
@@ -43,7 +44,7 @@ module.exports = function(api) {
       [
         '@babel/plugin-proposal-class-properties',
         {
-          loose: isDevelopmentEnv || isTestEnv
+          loose: true
         }
       ],
       [
@@ -53,19 +54,19 @@ module.exports = function(api) {
         }
       ],
       [
-        '@babel/plugin-transform-runtime',
+        '@babel/plugin-transform-private-methods',
         {
-          helpers: false,
-          regenerator: true,
-          corejs: false
+          loose: true
         }
       ],
       [
-        '@babel/plugin-transform-regenerator',
+        '@babel/plugin-transform-private-property-in-object',
         {
-          async: false
+          loose: true
         }
-      ]
+      ],
+      '@babel/plugin-transform-runtime',
+      '@babel/plugin-transform-regenerator'
     ].filter(Boolean)
   };
 };
