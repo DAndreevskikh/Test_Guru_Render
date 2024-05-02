a47697172287b93d0def326ce46cc5b6b2baaeb8
Dmitry <133801191+DAndreevskikh@users.noreply.github.com>
2024-05-02 14:11:35 +0300

    Update render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,7 +6,7 @@ bundle install
 yarn install
 bundle exec rake assets:precompile
 #bundle exec rake assets:clean
-bundle exec rake db:migrate:down VERSION=20240424113250
-bundle exec rake db:migrate:up VERSION=20240424113250
+#bundle exec rake db:migrate:down VERSION=20240424113250
+#bundle exec rake db:migrate:up VERSION=20240424113250
 bundle exec rake db:migrate
 #bundle exec rake db:seed
